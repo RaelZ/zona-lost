9143db127904cbd3797e041d8b987995f951828f
Israel Ferreira <48022462+RaelZ@users.noreply.github.com>
2022-09-14 15:17:57 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PathRoutes from './PathRoutes';
 import './global.css'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function App() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(location.pathname === '/') navigate('/home')
+  }, [location.pathname, navigate])
+
   return (
     <PathRoutes />
   );
--- src/PathRoutes.tsx
+++ src/PathRoutes.tsx
@@ -1,19 +1,17 @@
 import React from "react"
 import { routes } from "./routes"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 
 const PathRoutes: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          {routes.map(({ id, path, component }) => (
-            <Route key={id} path={path} element={component} />
-          ))}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        {routes.map(({ id, path, component }) => (
+          <Route key={id} path={path} element={component} />
+        ))}
+      </Route>
+    </Routes>
   )
 }
 
